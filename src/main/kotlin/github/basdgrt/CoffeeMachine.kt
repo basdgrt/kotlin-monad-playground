8b94809a4d0adefb3c548cee9be35c667512a85d
Bas de Groot <bdegroot@protonmail.com>
2022-04-20 21:22:43 +0200

    first commit

---
+++ src/main/kotlin/github/basdgrt/CoffeeMachine.kt
@@ -0,0 +1,14 @@
+package github.basdgrt
+
+class CoffeeMachine {
+    fun makeCoffee(): Coffee {
+        val beans = grindBeans()
+        return brew(beans)
+    }
+}
+
+private fun grindBeans(): CoffeeBeans = TODO()
+private fun brew(beans: CoffeeBeans): Coffee = TODO()
+
+object Coffee
+object CoffeeBeans
---
+++ src/main/kotlin/github/basdgrt/Either.kt
@@ -0,0 +1,26 @@
+package github.basdgrt
+
+sealed class Either<out A, out B> {
+    data class Failure<out A>(val value: A) : Either<A, Nothing>()
+    data class Success<out B>(val value: B) : Either<Nothing, B>()
+}
+
+inline fun <A, B, C> Either<A, B>.map(fn: (B) -> C) = when (this) {
+    is Either.Failure -> this
+    is Either.Success -> Either.Success(fn(this.value))
+}
+
+inline fun <A, B, C> Either<A, B>.flatMap(fn: (B) -> Either<A, C>) = when (this) {
+    is Either.Failure -> this
+    is Either.Success -> fn(this.value)
+}
+
+inline fun <A, B, C> Either<A, B>.fold(onError: (A) -> C, onSuccess: (B) -> C) = when (this) {
+    is Either.Failure -> onError(value)
+    is Either.Success -> onSuccess(value)
+}
+
+inline fun <A, B> Either<A, B>.onFailure(function: (Either.Failure<A>) -> Nothing): B = when(this) {
+    is Either.Failure -> function(this)
+    is Either.Success -> value
+}
