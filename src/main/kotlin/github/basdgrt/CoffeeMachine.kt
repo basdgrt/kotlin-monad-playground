d86a3957140bd7a7a79dbfe579f6114086c414c9
Bas de Groot <bdegroot@protonmail.com>
2022-11-16 15:00:10 +0100

    Better example

--- src/main/kotlin/github/basdgrt/CoffeeMachine.kt
+++ src/main/kotlin/github/basdgrt/CoffeeMachine.kt
@@ -2,62 +2,38 @@ package github.basdgrt
 
 import arrow.core.continuations.EffectScope
 import arrow.core.continuations.effect
+import arrow.core.continuations.either
 import arrow.fx.coroutines.parZip
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
-import kotlin.time.Duration.Companion.seconds
+
 
 class CoffeeMachine {
     context (EffectScope<MachineFailure>)
-    suspend fun makeCoffee(): Coffee =
-        parZip(
-            Dispatchers.IO,
-            { grindBeans() },
-            { boilWater() }
-        ) { beans, water -> brew(beans, water) }
+    suspend fun makeCoffee(): Coffee = parZip(
+        Dispatchers.IO,
+        { grindBeans() },
+        { boilWater() }
+    ) { beans, water -> brew(beans, water) }
 
-    private suspend fun EffectScope<MachineFailure>.brew(beans: CoffeeBeans, water: Water): Coffee {
-        println("Started brewing")
-        delay(1.seconds)
-        println("finished brewing")
-        return Coffee
-    }
-
-    private suspend fun EffectScope<MachineFailure>.grindBeans(): CoffeeBeans {
-        println("started grinding beans")
-        delay(2.seconds)
-
-        if ((1..100).random() < 20) {
-            return shift(MachineFailure.NotEnoughBeans)
-        }
-
-        println("finished grinding beans")
-        return CoffeeBeans
-    }
-
-    private suspend fun EffectScope<MachineFailure>.boilWater(): Water {
-        println("start boling water")
-        delay(1.seconds)
-
-        if ((1..100).random() > 20) {
-            return shift(MachineFailure.MissingFilter)
-        }
-
-        println("finished boiling water")
-        return Water
-    }
+    private suspend fun EffectScope<MachineFailure>.grindBeans(): CoffeeBeans = TODO()
+    private suspend fun EffectScope<MachineFailure>.boilWater(): Water = TODO()
+    private suspend fun EffectScope<MachineFailure>.brew(beans: CoffeeBeans, water: Water): Coffee = TODO()
 }
 
 class Barista(private val machine: CoffeeMachine) {
-    suspend fun handleOrder() {
-       effect { machine.makeCoffee() }
-           .toEither()
-           .fold(
-               ifRight = { println("Huge success")},
-               ifLeft = { println("Failed with $it")}
-           )
-    }
+    suspend fun handleOrderFoo() = effect { machine.makeCoffee() }
+        .fold(
+            transform = { println("Huge success") },
+            recover = { /* pattern match over failures */ },
+            error = { /* do something with exception */ }
+        )
+
+    suspend fun handleOrder() = either { machine.makeCoffee() }
+        .fold(
+            ifRight = { println("Huge success") },
+            ifLeft = { /* pattern match over failures */ }
+        )
 }
 
 fun main() = runBlocking {
